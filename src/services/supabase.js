31bf197b68d151c05d727a6eafe506972e220316
Anushk Jain <anushk2201@gmail.com>
2024-07-29 00:29:56 +0530

    configured: supabase.

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://ohvjyoqeodripkouqqmz.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odmp5b3Flb2RyaXBrb3VxcW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxNTgxMjMsImV4cCI6MjAzNzczNDEyM30.vCB6MWhfRj_UCsPYF464xobre1gxZPmZzEVTD_c3Yl8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
