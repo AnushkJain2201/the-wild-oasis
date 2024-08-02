b32b7a5c9d0cc810932662b2f7c45e2a22cf0ed4
Anushk Jain <anushk2201@gmail.com>
2024-08-02 11:21:58 +0530

    feat: user can get cabins now.

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,11 +4,23 @@ export async function getCabins() {
     let { data, error } = await supabase
         .from('cabins')
         .select('*')
-    
-    if(error) {
+
+    if (error) {
         console.error(error)
         throw new Error('Cabins could not be fetched')
     }
 
     return data;
+}
+
+export async function deleteCabin(id) {
+    const { error } = await supabase
+        .from('cabins')
+        .delete()
+        .eq('id', id)
+
+    if (error) {
+        console.error(error)
+        throw new Error('Cabin could not be deleted')
+    }
 }
