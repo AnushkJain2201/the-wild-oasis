e2220b0f1a0e590ef63acfab6ce89dfabbc04a63
Anushk Jain <anushk2201@gmail.com>
2024-08-08 17:33:10 +0530

    feat: user can create cabins now.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,14 @@
 // import { useEffect } from "react";
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 // import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+	const [showForm, setShowForm] = useState(false);
 	return (
 		<>
 			<Row type='horizontal'>
@@ -14,6 +18,8 @@ function Cabins() {
 
 			<Row>
 				<CabinTable />
+				{showForm && <CreateCabinForm />}
+				<Button onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
 			</Row>
 		</>
 	);
